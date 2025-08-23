3ba4f9c31ae0660b81bc10239558f83ae32019aa
Ross Large <ross_large@msn.com>
2025-08-23 12:50:44 +0100

    Initial Commit

---
+++ TideTimes/TideTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TideTimesApp.swift
+//  TideTimes
+//
+//  Created by Ross Large on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TideTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TideTimesTests/TideTimesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TideTimesTests.swift
+//  TideTimesTests
+//
+//  Created by Ross Large on 23/08/2025.
+//
+
+import Testing
+@testable import TideTimes
+
+struct TideTimesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
